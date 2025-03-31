e5e092eb0d1cabb9e240ddf6673ffa6280d1eaa4
Sandeep <your-email@example.com>
2025-03-31 15:58:14 +0530

    add new file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
 console.log("komal");
+
+let notesContainer = document.querySelector(".notes-container") ; 
+let createbtn = document.querySelector(".btn") ; 
+let notes = document.querySelectorAll(".input-box");
+
+
+createbtn.addEventListener('click' , function () {
+    let inputBox = document.createElement('p');
+    let img = document.createElement("img");
+    inputBox.classList = "input-box" ;
+    inputBox.setAttribute("contenteditable" , "true") ;
+    img.src = "./assets/delete.png" ; 
+    notesContainer.appendChild(inputBox).appendChild(img);
+
+})
+
+
+
+notesContainer.addEventListener('click' , function (e) {
+    if (e.target.tagName === "IMG") {
+        e.target.parentElement.remove();
+        updateStorage();    
+    }
+    else if (e.target.tagName === "P"){
+        notes = document.querySelectorAll(".input-box") ;
+        notes.forEach( nt =>{
+            nt.onkeyup = function (){
+        updateStorage();    
+                
+            }
+        })
+    }
+})
+
+
+function updateStorage() {
+    localStorage.setItem("notes" , notesContainer.innerHTML);
+}
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes")
+}
+showNotes()
