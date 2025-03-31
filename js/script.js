c31723886720baa6f91ee719cd3cef4aed7d6e66
Sandeep <your-email@example.com>
2025-03-31 15:59:38 +0530

    add new file

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
 console.log("komal");
 
-let notesContainer = document.querySelector(".notes-container") ; 
-let createbtn = document.querySelector(".btn") ; 
+let notesContainer = document.querySelector(".notes-container");
+let createbtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
 
-createbtn.addEventListener('click' , function () {
+createbtn.addEventListener('click', function () {
     let inputBox = document.createElement('p');
     let img = document.createElement("img");
-    inputBox.classList = "input-box" ;
-    inputBox.setAttribute("contenteditable" , "true") ;
-    img.src = "./assets/delete.png" ; 
+    inputBox.classList = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+    img.src = "./assets/delete.png";
     notesContainer.appendChild(inputBox).appendChild(img);
 
 })
 
 
 
-notesContainer.addEventListener('click' , function (e) {
+notesContainer.addEventListener('click', function (e) {
     if (e.target.tagName === "IMG") {
         e.target.parentElement.remove();
-        updateStorage();    
+        updateStorage();
     }
-    else if (e.target.tagName === "P"){
-        notes = document.querySelectorAll(".input-box") ;
-        notes.forEach( nt =>{
-            nt.onkeyup = function (){
-        updateStorage();    
-                
+    else if (e.target.tagName === "P") {
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function () {
+                updateStorage();
+
             }
         })
     }
@@ -35,7 +35,7 @@ notesContainer.addEventListener('click' , function (e) {
 
 
 function updateStorage() {
-    localStorage.setItem("notes" , notesContainer.innerHTML);
+    localStorage.setItem("notes", notesContainer.innerHTML);
 }
 function showNotes() {
     notesContainer.innerHTML = localStorage.getItem("notes")
